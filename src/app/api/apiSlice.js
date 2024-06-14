a1345fe8223ed3ee6fb2e5e1fcd58dc27cf04dde
edhuva <edhuva491@gmail.com>
2024-06-14 22:35:32 +0200

    updating - code ready to deploy frontend

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -4,7 +4,6 @@ import { setCredentials } from '../../features/auth/authSlice';
 //fetch Base Query 
 const baseQuery =  fetchBaseQuery({ 
     baseUrl: 'https://kgtechrepairs-api-v2.onrender.com',
-    // baseUrl: 'http://localhost:3500',
     credentials: 'include',
     prepareHeaders: (hearders, { getState }) => {
         const token = getState().auth.token
